3190d4a7ec3c7823a16eb7b28b394b3f3abac404
scelogumede95@gmail.com <scelogumede95@gmail.com>
2024-07-23 01:15:35 +0200

    done

--- src/components/SubMenu.jsx
+++ src/components/SubMenu.jsx
@@ -18,7 +18,7 @@ const SubMenu = () => {
   return (
     <div ref={menu} className={`${subMenu ? "absolute -translate-x-1/2  bg-white transparent hover:bg-white rounded-md p-4 border border-solid border-red-400" :"h-0" }`}>
         {links?.length>0 && subMenu && links.map((link,index)=>{
-            return <p key={index} className="text-slate-600 cursor-pointer  text-sm p-2 hover:bg-blue-100  ">{link.label}</p>
+            return <p key={index} className="text-slate-600 border-b-[1px] border-b-solid border-b-transparent hover:border-b-red-600 cursor-pointer hover:indent-[2px] duration-500 transition-all text-sm p-2 hover:bg-red-100  ">{link.label}</p>
         })}
     </div>
   )
